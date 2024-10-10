68de0dd72b7ac5d154e4a95f8376bf7236e40d36
Jzin23 <joaohenriquedepaulacosta@gmail.com>
2024-10-10 12:46:34 -0400

    Reparos

--- app/Http/Controllers/AdmPerfilController.php
+++ app/Http/Controllers/AdmPerfilController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class AdmArtigosController extends Controller
+class AdmPerfilController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -13,7 +13,7 @@ class AdmArtigosController extends Controller
      */
     public function index()
     {
-        return view('admin.ArtigoAdmin');
+        return view('admin.PerfilAdmin');
     }
 
     /**
